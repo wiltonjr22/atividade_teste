3c6de5160f5503aa974ef104f5aca04a3c7009e9
wilton junior <wilton.junior@newavetelecom.com.br>
2021-05-01 19:57:36 -0300

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const { getUsers, createUser, getUser, deleteUser, updateUser }  = require ('../controllers/users.js')
+
+const router = express.Router();
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+exports.modules = router
