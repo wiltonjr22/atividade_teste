b35efa4c0137207f16e0a5bc570b65d52051b0ea
wilton junior <49769357+wiltonjr22@users.noreply.github.com>
2021-05-02 20:43:16 -0300

    Controller finished
    terminando controler e iniciando documentaçao

--- controllers/users.js
+++ controllers/users.js
@@ -4,55 +4,56 @@ module.exports = {
     async create(request, response) {
         try {
             const result = await users.create(request.body);
-            response.status(201);
-            response.json(result);
+            response.status(201)
+            response.json(result)
         } catch (error) {
-            response.status(500);
-            response.json(JsonError(request, response, 'Não foi possível adicionar o user'));
+            response.status(500)
+            response.json(JsonError(request, response, 'Não foi possível adicionar o user'))
         }
     },
 
     async read(request, response) {
         try {
-            const result = await users.findAll({ raw: true });
-            response.json(result);
+            const result = await users.findAll({ raw: true })
+            response.json(result)
         } catch (error) {
-            response.status(500);
-            response.json(JsonError(request, response, 'Não foi possível buscar os users'));
+            response.status(500)
+            response.json(JsonError(request, response, 'Não foi possível buscar os users'))
         }
     },
 
     async update(request, response) {
         try {
-            const { id } = request.params;
-            const users = await users.findOne({ where: { id } });
-            if (users) {
-                await users.update(request.body);
-                response.json({ status: '200', message: 'Usuario atualizado com sucesso ' });
+            const { id } = request.params
+            const user = await users.findOne({ where: { id } })
+            if (user) {
+                await users.save(request.body)
+                response.json(user)
+                response.status(200)
             } else {
-                response.status(404);
-                response.json(JsonError(request, response, 'Usuario não encontrado'));
+                response.status(404)
+                response.json(JsonError(request, response, 'Usuario não encontrado'))
             }
         } catch (error) {
-            response.status(500);
-            response.json(JsonError(request, response, 'Não foi possível atualizar o user'));
+            response.status(500)
+            response.json(JsonError(request, response, 'Não foi possível atualizar o user'))
         }
     },
 
     async delete(request, response) {
-        const { id } = request.params;
+        const { id } = request.params
         try {
-            const users = await users.findOne({ where: { id } });
+            const users = await users.findOne({ where: { id } })
             if (users) {
                 await users.destroy();
-                response.json({ status: '200', message: 'Usuario deletado com sucesso' });
+                response.json({ status: '200', message: 'Usuario deletado com sucesso' })
             } else {
                 response.status(404);
-                response.json(JsonError(request, response, 'Usuario não encontrado'));
+                response.json(JsonError(request, response, 'Usuario não encontrado'))
             }
         } catch (error) {
             response.status(500);
-            response.json(JsonError(request, response, 'Não foi possível deletar o user'));
+            response.json(JsonError(request, response, 'Não foi possível deletar o user'))
         }
     }
 };
